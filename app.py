3342a0bb0b691ce9b081ee6b3a7e6e2f5d76fa96
Ridha Hassoun <ridha@ridha.me>
2021-08-02 22:59:04 -0700

    Wrote logic to handle batch conversions and updated docs.

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 CORS(app)
 
 
+def process_data(data):
+    conversion_dict = types[data["type"]]
+    conversion_from = conversion_dict[data["from"]["units"]]
+    conversion = conversion_from[data["to"]]
+
+    value = data["from"]["value"]
+    if not (isinstance(value, int) or isinstance(value, float)):
+        raise TypeError
+
+    if data["type"] == "temp":
+        result = conversion(value)
+    else:
+        result = conversion * data["from"]["value"]
+
+    data["result"] = result
+
+    return data
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -17,22 +36,14 @@ def api():
         data = request.get_json(silent=True)
         if data:
             try:
-                conversion_dict = types[data["type"]]
-                conversion_from = conversion_dict[data["from"]["units"]]
-                conversion = conversion_from[data["to"]]
-
-                value = data["from"]["value"]
-                if not (isinstance(value, int) or isinstance(value, float)):
-                    raise TypeError
-
-                if data["type"] == "temp":
-                    result = conversion(value)
+                if "multiple" in data:
+                    number_of_conversions = len(data) - 1
+                    for index in range(number_of_conversions):
+                        processed_data = process_data(data[str(index)])
+                        data[str(index)] = processed_data
+                    return data
                 else:
-                    result = conversion * data["from"]["value"]
-
-                data["result"] = result
-
-                return data
+                    return process_data(data)
             except KeyError as e:
                 return {"Error":
                         "Invalid value ({}) used. Check syntax/docs".format(e)}
